462c5bdf0f21bd1eecfe7cc39a66678451d0e5d2
ppchuchkov1 <ppchuchkov1@gmail.com>
2025-07-04 19:52:59 +0300

    Add deposite to user wallet via Stripe

--- app/wallet/wallet.routes.js
+++ app/wallet/wallet.routes.js
@@ -1,8 +1,23 @@
 const express = require("express");
-const router = express.Router();
-const { getWallet } = require("./wallet.controller");
+const {
+  getWallet,
+  createDepositCheckoutSession,
+  handleDepositWebhook,
+} = require("../controllers/wallet.controller");
 const verifyToken = require("../middleware/verifyToken");
 
+const router = express.Router();
+
 router.get("/", verifyToken, getWallet);
+router.post(
+  "/create-deposit-checkout-session",
+  verifyToken,
+  createDepositCheckoutSession
+);
+router.post(
+  "/webhook",
+  express.raw({ type: "application/json" }),
+  handleDepositWebhook
+);
 
 module.exports = router;
